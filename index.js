ad41af07ceb7582e98bfadc4cd7a7e97fdc97f12
stephen oshyn <scoshyn@gmail.com>
2023-08-27 21:46:09 -0700

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+//const appendCat
+
+function appendCat(name) {
+    name = [...cats, "Broom"];
+    return name;
+}
+
+function destructivelyAppendCat(names) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(names) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(names) {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(names) {
+    cats.shift();
+}
+
+function prependCat (name) {
+    name = ["Arnold", ...cats];
+    return name;
+}
+
+function removeLastCat (names) {
+    names = cats.slice(0, cats.length -1);
+    return names;
+}
+
+function removeFirstCat(names) {
+    names = cats.slice(1);
+    return names;
+}
